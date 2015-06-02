02c46812edf7201eb9c3f585c9cde9f450239567
seotts <se0042@gmail.com>
2015-06-02 11:55:46 -0400

    starting dictionary extension

---
+++ dictionary.js
@@ -0,0 +1,28 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+		['', 'use English dictionary','test'],
+		['', 'use %m.languages to English dictionary', 'test']
+		['r', 'definition of %s', 'test'],
+        ]
+
+	menus: {
+		languages: ['Spanish', 'Polish', 'Cherokee'],
+	}
+    };
+	
+   
+
+    // Register the extension
+    ScratchExtensions.register('Dictionary extension', descriptor, ext);
+})({});
