bba6e7231040571a760501d3f08c721342a6adea
seotts <se0042@gmail.com>
2015-06-04 16:05:26 -0400

    adding detectLanguage

---
+++ detectLanguage.js
@@ -0,0 +1,32 @@
+
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+  
+    ext._get_lang = function(text){
+	$.ajax({
+		url: 'http://ws.detectlanguage.com/0.2/detect?q='+text+ '&key=demo',
+		dataType:'json',
+		success: function(language_results){
+			langCode = language_results['data']['detections'][0]['language'];
+			callback(langCode);
+		}
+	});
+    };
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+		// Block type, block name, function name, param1 default value, param2 default value
+            ['r', 'language of %s', 'get_lang','Hello world"],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('testing', descriptor, ext);
+})({});
