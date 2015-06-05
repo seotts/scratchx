38e5f8b10abe73ff3860568e01db3751a217da0f
seotts <se0042@gmail.com>
2015-06-05 10:08:05 -0400

    debugging

--- detectLanguage.js
+++ detectLanguage.js
@@ -12,16 +12,16 @@
     };
   
     ext.get_lang = function(text){
-	apiKey='trnsl.1.1.20150605T132039Z.c660d54664d42d1d.3e02378e3b4f321ba39e2dee119909f8b402292a';	
-	$.ajax({
-		url: 'https://translate.yandex.net/api/v1.5/tr.json/detect?key='+APIkey+'&text='+text+'&callback=myCallback',
-		dataType:'jsonp',
-		success: function(language_results){
-			console.log('succeessssss');
-			langCode = 'en'
-			callback(langCode);
-		}
-	});
+	apiKey="trnsl.1.1.20150605T132039Z.c660d54664d42d1d.3e02378e3b4f321ba39e2dee119909f8b402292a";	
+//	$.ajax({
+//		url: "https://translate.yandex.net/api/v1.5/tr.json/detect?key="+APIkey+"&text="+text+"&callback=myCallback",
+//		dataType:'jsonp',
+//		success: function(language_results){
+//			console.log('succeessssss');
+//			langCode = 'en'
+//			callback(langCode);
+//		}
+//	});
     };
     // Block and block menu descriptions
     var descriptor = {
