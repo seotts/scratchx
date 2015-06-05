233a577e0c86831b88ab530a234e1ecbb669ef03
seotts <se0042@gmail.com>
2015-06-05 17:28:40 -0400

    Revert "adding translate block"
    This reverts commit d5b0846503db5c05db438a54bf435513cd13f443.

--- detectLanguage.js
+++ detectLanguage.js
@@ -14,9 +14,7 @@
     };
 	
     var langNames={"ar":"Arabic","az":"Azerbaijani","be":"Belarusian","bg":"Bulgarian","bs":"Bosnian","ca":"Catalan","cs":"Czech","da":"Danish","de":"German","el":"Greek","en":"English","es":"Spanish","et":"Estonian","fa":"Persian","fi":"Finnish","fr":"French","he":"Hebrew","hr":"Croatian","hu":"Hungarian","hy":"Armenian","id":"Indonesian","is":"Icelandic","it":"Italian","ja":"Japanese","ka":"Georgian","ko":"Korean","lt":"Lithuanian","lv":"Latvian","mk":"Macedonian","ms":"Malay","mt":"Maltese","nl":"Dutch","no":"Norwegian","pl":"Polish","pt":"Portuguese","ro":"Romanian","ru":"Russian","sk":"Slovak","sl":"Slovenian","sq":"Albanian","sr":"Serbian","sv":"Swedish","th":"Thai","tr":"Turkish","tt":"Tatar","uk":"Ukrainian","vi":"Vietnamese","zh":"Chinese"};
-    var names = [];
-    for(var n in langNames) names.push(n);
-
+    
     ext.get_lang = function(text, callback){
 	console.log('setting key');
 //	var apiKey="trnsl.1.1.20150605T132039Z.c660d54664d42d1d.3e02378e3b4f321ba39e2dee119909f8b402292a";
@@ -38,13 +36,8 @@
         blocks: [
 		// Block type, block name, function name, param1 default value, param2 default value
             ['R', 'language of %s', 'get_lang','Hello world'],
-	    ['R', 'translate %s from %m.langs to %m.langs', 'translate', 'Hola mundo', 'Spanish', 'English'],
         ]
     };
-	
-    menus: {
-  	langs: names,
-    }
 
     // Register the extension
     ScratchExtensions.register('Detect Language', descriptor, ext);
