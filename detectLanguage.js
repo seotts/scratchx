bfc5822ea7e5f4244a52391f696784495bb38c07
seotts <se0042@gmail.com>
2015-06-05 10:27:19 -0400

    comment out logs

--- detectLanguage.js
+++ detectLanguage.js
@@ -12,15 +12,14 @@
     };
   
     ext.get_lang = function(text){
-	console.log('setting key');
+	//console.log('setting key');
 	var apiKey="trnsl.1.1.20150605T132039Z.c660d54664d42d1d.3e02378e3b4f321ba39e2dee119909f8b402292a";
-	console.log('starting ajax');	
+	//console.log('starting ajax');	
 	$.ajax({
-		url: "https://translate.yandex.net/api/v1.5/tr.json/detect?key="+APIkey+"&text="+text+"&callback=myCallback",
-		
+		url: "https://translate.yandex.net/api/v1.5/tr.json/detect?key="+APIkey+"&text="+text+"&callback=myCallback",		
 		dataType:'jsonp',
 		success: function(language_results){
-			console.log('succeessssss');
+		//	console.log('succeessssss');
 			langCode = 'en'
 			callback(langCode);
 		}
