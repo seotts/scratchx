ab9a14de33394451b1566db14dee353584985be1
seotts <se0042@gmail.com>
2015-06-05 15:07:50 -0400

    remove console log

--- detectLanguage.js
+++ detectLanguage.js
@@ -14,13 +14,11 @@
     ext.get_lang = function(text){
 	//console.log('setting key');
 	var apiKey="trnsl.1.1.20150605T132039Z.c660d54664d42d1d.3e02378e3b4f321ba39e2dee119909f8b402292a";
-	//console.log('starting ajax');
-	return "this is a test";	
+	//console.log('starting ajax');	
 	$.ajax({
 		url: "https://translate.yandex.net/api/v1.5/tr.json/detect?key="+APIkey+"&text="+text+"&callback=myCallback",		
 		dataType:'jsonp',
 		success: function(language_results){
-		//	console.log('succeessssss');
 			langCode = 'en'
 			callback(langCode);
 		}
