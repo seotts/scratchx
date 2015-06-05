521c3b09050adfd0f6f5649731c53ea318891d81
seotts <se0042@gmail.com>
2015-06-05 09:29:08 -0400

    syntax fix

--- detectLanguage.js
+++ detectLanguage.js
@@ -12,7 +12,7 @@
     };
   
     ext.get_lang = function(text){
-	apiKey=trnsl.1.1.20150605T132039Z.c660d54664d42d1d.3e02378e3b4f321ba39e2dee119909f8b402292a;	
+	apiKey='trnsl.1.1.20150605T132039Z.c660d54664d42d1d.3e02378e3b4f321ba39e2dee119909f8b402292a';	
 	$.ajax({
 		url: 'https://translate.yandex.net/api/v1.5/tr.json/detect?key='+APIkey+'&text='+text+'&callback=myCallback',
 		dataType:'jsonp',
